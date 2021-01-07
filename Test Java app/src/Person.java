06be8ab21145ba5130f309a4c7764f51bba11039
Joel Jackson <74617125+jjackson318@users.noreply.github.com>
2021-01-07 14:18:24 -0600

    Add files via upload

---
+++ Test Java app/src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    String Name;
+    int age;
+    int height;
+}
---
+++ Test Java app/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//System.out.println("Hello World");
+      //  System.out.print("Hi");
+        //System.out.println("Hey");
+        Scanner input = new Scanner(System.in);
+        //System.out.println("What is your Name");
+        //String firstName = input.nextLine();
+//        System.out.println("Enter a name");
+//        String name = input.nextLine();
+//        System.out.println("Give me an adverb");
+//        String adverb = input.nextLine();
+//        System.out.println("Give ma a adjective");
+//        String adjective = input.nextLine();
+//        System.out.println("Give me a verb");
+//        String verb = input.nextLine();
+//        System.out.println("Give me a noun two: ");
+//        String nounTwo = input.nextLine();
+//        System.out.println("Give me a number: ");
+//        String number = input.nextLine();
+//        System.out.println("Give me your name: ");
+//        String yourName = input.nextLine();
+//
+//        System.out.printf("Here is your story: \n");
+//        System.out.printf("\n Dear %s,", name);
+//        System.out.printf("\n You are %s %s and I want to be your %s!", adverb, adjective, verb);
+//        System.out.printf("\n I want to go to the %s with you in %s days.", nounTwo, number);
+//        System.out.printf("\n Sincerely, %s \n", yourName);
+
+
+        byte b = (byte)1234;
+
+         double num = 5;
+ double num2 = 10;
+
+
+    boolean result =  num % num2 == 0;
+
+        System.out.println(result);
+
+        System.out.printf("Here is your story: \n");
+        System.out.printf("\n Dear %s,", name);
+        System.out.printf("\n You are %s %s and I want to be your %s!", adverb, adjective, verb);
+        System.out.printf("\n I want to go to the %s with you in %s days.", nounTwo, number);
+        System.out.printf("\n Sincerely, %s \n", yourName);
+    }
+}
